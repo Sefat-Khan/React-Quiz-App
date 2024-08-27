c01a1d7c7c7c3a95fd989c0ffe0c0e22fed73333
Sefat Khan <149791936+Sefat-Khan@users.noreply.github.com>
2024-08-27 11:03:27 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import vitePluginRequire from "vite-plugin-require";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginRequire.default()],
+  plugins: [react(),],
 });
