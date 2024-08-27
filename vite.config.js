fbb2d13aa0b87a46c0b012ef7b018a2942adb631
Sefat Khan <149791936+Sefat-Khan@users.noreply.github.com>
2024-08-27 11:54:17 +0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import requireTransform from "vite-plugin-require";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    requireTransform({
-      // your options here
-    }),
-  ],
+  plugins: [react()],
 });
